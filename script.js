55f5182bb1fdf9c7ac2c67ab993c0a264209ff03
naimdeb <naimdebard02@gmail.com>
2024-11-28 11:37:35 +0100

    added basic add to list

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+const inputAjoutNom = document.querySelector(".inputAjoutNom")
+const btnAjoutNom = document.querySelector(".btnAjoutNom")
+const listeNoms = document.querySelector(".listeNom ul")
+
+
+btnAjoutNom.addEventListener('click', ajouterNomAListe)
+
+function ajouterNomAListe(){
+
+    let liNom = document.createElement('li');
+
+    liNom.textContent = inputAjoutNom.value
+
+    listeNoms.appendChild(liNom)
+
+    inputAjoutNom.value = ''
+    
+}
