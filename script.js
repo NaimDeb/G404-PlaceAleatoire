3331a51b8f5d3f2f5969093a82541e2caec742bb
naimdeb <naimdebard02@gmail.com>
2024-11-28 15:45:59 +0100

    added way to add to div

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 const shuffleButton = document.querySelector(".shuffleButton")
 const listeNoms = document.querySelector(".listeNoms")
-const listShuffled = document.querySelector(".listShuffled")
+const listShuffled = document.querySelector(".listeShuffled")
 
 let listeDesNoms = []
 
-listeNoms.addEventListener('input', ajouterNomAListe)
 
+listeNoms.addEventListener('input', ajouterNomAListe)
 // Récupère les noms donnés dans la div listeNom et l'ajoute a la liste listeDesNoms
 function ajouterNomAListe(){
     listeDesNoms = []
@@ -14,15 +14,14 @@ function ajouterNomAListe(){
     listeNoms.querySelectorAll("div").forEach((nom) => {
         listeDesNoms.push(nom.textContent) 
     })
-console.log(listeDesNoms);
-
 }
 
 
 shuffleButton.addEventListener("click", handleClickShuffle)
-
+// Quand on clique sur le bouton shuffle, randomise la liste et l'ajoute quelquepart
 function handleClickShuffle() {
-    shuffleArray(listeDesNoms)
+    
+    showList(shuffleArray(listeDesNoms), listShuffled)
 }
 
 
@@ -31,18 +30,32 @@ function shuffleArray(array) {
 
     console.log("Shuffling...");
     
+    const newArray = array
 
     for (let i = array.length-1 ; i> 0 ; i--) {
+
         const j = Math.floor( Math.random() * (i+1) )
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        const temp = newArray[i];
+        newArray[i] = newArray[j];
+        newArray[j] = temp;
 
     }
 
-    console.log(array);
-    
+    return newArray;
 
 }
 
-console.log(listeDesNoms);
+// Prend une liste array et ajoute chacun de ses trucs dans l'HTML a l'endroit placeToAdd
+function showList(array, placeToAdd) {
+    console.log("Showing list...");
+     
+    array.forEach(element => {
+
+        const newDiv = document.createElement("p")
+        newDiv.innerHTML = element
+        placeToAdd.appendChild(newDiv)
+        
+    });1
+    
+}
+
