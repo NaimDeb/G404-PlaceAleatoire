31f1df9d08eb61f4f38f80bba88181b8c93b1fe8
gaksss <clement.carlier2@gmail.com>
2024-11-29 16:40:31 +0100

    modal closing + tableNChair counter

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 const start = document.querySelector("#start");
-const modal = document.querySelector("#modal")
-const fullPage = document.querySelector("#fullPage")
-const bookMark = document.querySelector("#bookMark")
-const bookMarkContent = document.querySelector(".bookmarkContent")
-const nameChange = document.querySelector("#nameChange")
+const modal = document.querySelector("#modal");
+const fullPage = document.querySelector("#fullPage");
+const bookMark = document.querySelector("#bookMark");
+const bookMarkContent = document.querySelector(".bookmarkContent");
+const nameChange = document.querySelector("#nameChange");
+const tablePlus = document.querySelector(".tablePlus");
+const chairPlus = document.querySelector(".chairPlus");
+const tableCount = document.querySelector("#tableCount");
+const chairCount = document.querySelector("#chairCount");
 
 start.addEventListener("click", handleModalClose);
 bookMark.addEventListener("click", handleBookMarkOpen);
+tablePlus.addEventListener("click", handleCountPlus);
+chairPlus.addEventListener("click", handleCountPlus);
 
-
-
-function handleModalClose(event){
-    console.log(event);
-    fullPage.classList.remove("hidden")
-    fullPage.classList.add("flex")
-    modal.classList.remove("flex")
-    modal.classList.add("hidden")
-    
+function handleModalClose(event) {
+  fullPage.classList.remove("hidden");
+  fullPage.classList.add("flex");
+  modal.classList.remove("flex");
+  modal.classList.add("hidden");
 }
 
-function handleBookMarkOpen(event){
-    bookMark.classList.add("h-[750px]")
-    bookMarkContent.classList.remove("hidden")
-    bookMarkContent.classList.add("flex")
-    nameChange.classList.add("hidden")
+function handleBookMarkOpen(event) {
+  bookMark.classList.add("h-[750px]");
+  bookMarkContent.classList.remove("hidden");
+  bookMarkContent.classList.add("flex");
+  nameChange.classList.add("hidden");
+}
+let countTable = 0;
+let countChair = 0;
+function handleCountPlus(event) {
+  if (event.target == tablePlus) {
+    countTable++;
+    tableCount.innerHTML = countTable;
+  } else if (event.target == chairPlus) {
+    countChair++;
+    chairCount.innerHTML = countChair;
+  }
 }
